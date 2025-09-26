38445262ddb6c564f9dca658ffb4a437504c1774
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-26 12:07:22 +0530

    fix the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/RAG_frontend/',
   plugins: [react()],
+  base: '/RAG_frontend/',
+  build: {
+    outDir: 'dist',
+  }
+
 })
