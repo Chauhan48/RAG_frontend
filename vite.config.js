e618ec6bc7618e42646ab1496e5c685c5c80e238
Anshit Chauhan <chauhananshit48@gmail.com>
2025-09-26 12:03:37 +0530

    fix the base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://github.com/Chauhan48/RAG_frontend.git',
+  base: '/RAG_frontend/',
   plugins: [react()],
 })
